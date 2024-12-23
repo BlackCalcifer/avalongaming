cef13255023f7ae84360800b5df9401cf511c1cd
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 17:39:00 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,14 +1,13 @@
 const datos = [
-    { 
-        nombre: "Cieitos-Otatrom", 
-        imagen: "recursos/Cieitos-Otatrom.png", 
+    {
+        nombre: "Cieitos-Otatrom",
+        imagen: "recursos/Cieitos-Otatrom.png",
         recursos: [
-            { id: "A", cantidad: 1 },
-            { id: "D", cantidad: 2 },
-            { id: "M", cantidad: 3 },
-            { id: "PI", cantidad: 1 },
-            { id: "V", cantidad: 2 }
-        ]
-    }
-    // Puedes agregar más objetos de mapa si es necesario
+            { tipo: "A", cantidad: 1 },
+            { tipo: "D", cantidad: 2 },
+            { tipo: "M", cantidad: 3 },
+            { tipo: "PI", cantidad: 1 },
+            { tipo: "V", cantidad: 2 },
+        ],
+    },
 ];
