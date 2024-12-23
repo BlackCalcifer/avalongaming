0ad4cb7428cd839793d5e938c3cd4fc4246db85a
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 14:07:11 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -4,15 +4,15 @@ const datos = [
         nombre: "Cieitos-Otatrom", 
         imagen: "recursos/Cieitos-Otatrom.png", 
         recursos: {
-            recurso1: { imagen: "recursos/A.PNG", cantidad: 1 }, 
-            recurso2: { imagen: "recursos/AL.PNG", cantidad: 0 }, 
-            recurso3: { imagen: "recursos/D.PNG", cantidad: 2 }, 
-            recurso4: { imagen: "recursos/E.PNG", cantidad: 0 }, 
-            recurso5: { imagen: "recursos/M.PNG", cantidad: 3 }, 
-            recurso6: { imagen: "recursos/MI.PNG", cantidad: 0 }, 
-            recurso7: { imagen: "recursos/PI.PNG", cantidad: 1 }, 
-            recurso8: { imagen: "recursos/R.PNG", cantidad: 0 }, 
-            recurso9: { imagen: "recursos/V.PNG", cantidad: 2 }
+            recurso1: { imagen: "nodos/A.PNG", cantidad: 1 }, 
+            recurso2: { imagen: "nodos/AL.PNG", cantidad: 0 }, 
+            recurso3: { imagen: "nodos/D.PNG", cantidad: 2 }, 
+            recurso4: { imagen: "nodos/E.PNG", cantidad: 0 }, 
+            recurso5: { imagen: "nodos/M.PNG", cantidad: 3 }, 
+            recurso6: { imagen: "nodos/MI.PNG", cantidad: 0 }, 
+            recurso7: { imagen: "nodos/PI.PNG", cantidad: 1 }, 
+            recurso8: { imagen: "nodos/R.PNG", cantidad: 0 }, 
+            recurso9: { imagen: "nodos/V.PNG", cantidad: 2 }
         }
     }
     // Puedes agregar más objetos de mapa si es necesario
