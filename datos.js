80653102665b2972c27098b241d6cbc85864c6f6
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 15:29:47 -0500

    Add files via upload

--- datos.js
+++ datos.js
@@ -4,15 +4,15 @@ const datos = [
         nombre: "Cieitos-Otatrom", 
         imagen: "recursos/Cieitos-Otatrom.png", 
         recursos: {
-            recurso1: { imagen: "A.PNG", cantidad: 1 }, 
-            recurso2: { imagen: "AL.PNG", cantidad: 0 }, 
-            recurso3: { imagen: "D.PNG", cantidad: 2 }, 
-            recurso4: { imagen: "E.PNG", cantidad: 0 }, 
-            recurso5: { imagen: "M.PNG", cantidad: 3 }, 
-            recurso6: { imagen: "MI.PNG", cantidad: 0 }, 
-            recurso7: { imagen: "PI.PNG", cantidad: 1 }, 
-            recurso8: { imagen: "R.PNG", cantidad: 0 }, 
-            recurso9: { imagen: "V.PNG", cantidad: 2 }
+            recurso1: { imagen: "nodos/A.PNG", cantidad: 1 }, 
+            recurso2: { imagen: "nodos/AL.PNG", cantidad: 0 }, 
+            recurso3: { imagen: "nodos/D.PNG", cantidad: 2 }, 
+            recurso4: { imagen: "nodos/E.PNG", cantidad: 0 }, 
+            recurso5: { imagen: "nodos/M.PNG", cantidad: 3 }, 
+            recurso6: { imagen: "nodos/MI.PNG", cantidad: 0 }, 
+            recurso7: { imagen: "nodos/PI.PNG", cantidad: 1 }, 
+            recurso8: { imagen: "nodos/R.PNG", cantidad: 0 }, 
+            recurso9: { imagen: "nodos/V.PNG", cantidad: 2 }
         }
     }
     // Puedes agregar más objetos de mapa si es necesario
