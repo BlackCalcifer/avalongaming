7058e4113152787825846622edbeaabfcc15a7f4
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 13:39:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-// Función para buscar el mapa
+// datos.js
+const datos = [
+    { 
+        nombre: "Cieitos-Otatrom", 
+        imagen: "recursos/Cieitos-Otatrom.png", 
+        recursos: {
+            recurso1: { imagen: "nodos/A.PNG", cantidad: 1 }, 
+            recurso2: { imagen: "nodos/AL.PNG", cantidad: 0 }, 
+            recurso3: { imagen: "nodos/D.PNG", cantidad: 2 }, 
+            recurso4: { imagen: "nodos/E.PNG", cantidad: 0 }, 
+            recurso5: { imagen: "nodos/M.PNG", cantidad: 3 }, 
+            recurso6: { imagen: "nodos/MI.PNG", cantidad: 0 }, 
+            recurso7: { imagen: "nodos/PI.PNG", cantidad: 1 }, 
+            recurso8: { imagen: "nodos/R.PNG", cantidad: 0 }, 
+            recurso9: { imagen: "nodos/V.PNG", cantidad: 2 }
+        }
+    }
+    // Puedes agregar más objetos de mapa si es necesario
+];// Función para buscar el mapa
 function buscar() {
     const query = document.getElementById('search-bar').value.toLowerCase();
     const resultados = document.getElementById('resultados');
