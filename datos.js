8299bfb95219e39054588231bbdcf6d76ce07fa4
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 15:52:19 -0500

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,14 @@
+const datos = [
+    { 
+        nombre: "Cieitos-Otatrom", 
+        imagen: "recursos/Cieitos-Otatrom.png", 
+        recursos: [
+            { id: "A", cantidad: 1 },
+            { id: "D", cantidad: 2 },
+            { id: "M", cantidad: 3 },
+            { id: "PI", cantidad: 1 },
+            { id: "V", cantidad: 2 }
+        ]
+    }
+    // Puedes agregar más objetos de mapa si es necesario
+];
---
+++ recursos.js
@@ -0,0 +1,11 @@
+const recursos = {
+    A: { id: "A", imagen: "nodos/A.PNG" },
+    AL: { id: "AL", imagen: "nodos/AL.PNG" },
+    D: { id: "D", imagen: "nodos/D.PNG" },
+    E: { id: "E", imagen: "nodos/E.PNG" },
+    M: { id: "M", imagen: "nodos/M.PNG" },
+    MI: { id: "MI", imagen: "nodos/MI.PNG" },
+    PI: { id: "PI", imagen: "nodos/PI.PNG" },
+    R: { id: "R", imagen: "nodos/R.PNG" },
+    V: { id: "V", imagen: "nodos/V.PNG" }
+};
