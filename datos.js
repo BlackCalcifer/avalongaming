e7d177a77839bab089b83a40d8f9c59f663a545f
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 21:07:20 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -10,7 +10,7 @@ const datos = [
     },
     {
         nombre: "Cilos-Otatrom t8",
-        imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320932396765417534/Cilos-Otatrom.png?ex=676b65a9&is=676a1429&hm=adc33095fbe6ed08caa212e5635b9c78366388cb7f44ff8098b60406f5d070ca&=&format=webp&quality=lossless&width=473&height=350", // Ruta remota
+        imagen: "recursos/Cieitos-Otatrom.png",
         recursos: [
             { tipo: "D", cantidad: 2 },
             { tipo: "V", cantidad: 6 },
@@ -19,7 +19,7 @@ const datos = [
     },
     {
         nombre: "Fasites-Azazsum t8",
-        imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320932868909699233/Fasites-Azazsum_t8.png?ex=676b6619&is=676a1499&hm=4ca2f7dd85e78b01385d0c617afdd23b1acae1a6e786c4132e2c163df635d88d&=&format=webp&quality=lossless&width=960&height=540", // Ruta remota
+        imagen: "recursos/Cieitos-Otatrom.png",
         recursos: [
             { tipo: "D", cantidad: 2 },
             { tipo: "V", cantidad: 6 },
@@ -28,9 +28,7 @@ const datos = [
     },
     {
         nombre: "Ferites-Ohohtum t8",
-        imagen: "https://cdn.discordapp.com/attachments/1320932318109630464/1320934561793704016/Ferites-Ohohtum_t8.png?ex=676b67ad&is=676a162d&hm=87e3cccfde4d9ec97d106372dc0c4c2c67a6b01d111b099e3ef9bce034c66a7f&", // Ruta remota
-        recursos: [
-            { tipo: "D", cantidad: 2 },
+        imagen: "recursos/Cieitos-Otatrom.png",
             { tipo: "V", cantidad: 6 },
             { tipo: "M", cantidad: 2 },
         ],
