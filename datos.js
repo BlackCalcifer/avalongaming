f9fbabfee6dabe11df868a899f968ec3e34a5481
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 21:22:04 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -8,24 +8,4 @@ const datos = [
             { tipo: "M", cantidad: 2 },
         ],
     },
-    {
-        nombre: "Cilos-Otatrom t8",
-        imagen: "recursos/Cieitos-Otatrom.png",
-        recursos: [
-            { tipo: "D", cantidad: 2 },
-            { tipo: "V", cantidad: 6 },
-            { tipo: "M", cantidad: 2 },
-        ],
-    },
-    {
-        nombre: "Cieitos-Otatrom",
-        imagen: "recursos/Cieitos-Otatrom.png",
-        recursos: [
-            { tipo: "A", cantidad: 1 },
-            { tipo: "D", cantidad: 2 },
-            { tipo: "M", cantidad: 3 },
-            { tipo: "PI", cantidad: 1 },
-            { tipo: "V", cantidad: 2 },
-        ],
-    },
 ];
