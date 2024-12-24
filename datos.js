204456c578eb12da2d4cbb02fe91933d4cc7b6f6
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 21:49:04 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -21,18 +21,55 @@ const datos = [
         nombre: "Fasites-Azazsum t8",
         imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320932868909699233/Fasites-Azazsum_t8.png?ex=676b6619&is=676a1499&hm=4ca2f7dd85e78b01385d0c617afdd23b1acae1a6e786c4132e2c163df635d88d&=&format=webp&quality=lossless&width=960&height=540", // Ruta remota
         recursos: [
-            { tipo: "D", cantidad: 2 },
-            { tipo: "V", cantidad: 6 },
-            { tipo: "M", cantidad: 2 },
+            { tipo: "V", cantidad: 7 },
+            { tipo: "MI", cantidad: 2 },
+			{ tipo: "AL", cantidad: 1 },
         ],
     },
     {
         nombre: "Ferites-Ohohtum t8",
         imagen: "https://cdn.discordapp.com/attachments/1320932318109630464/1320934561793704016/Ferites-Ohohtum_t8.png?ex=676b67ad&is=676a162d&hm=87e3cccfde4d9ec97d106372dc0c4c2c67a6b01d111b099e3ef9bce034c66a7f&", // Ruta remota
         recursos: [
-            { tipo: "D", cantidad: 2 },
+            { tipo: "V", cantidad: 6 },
+            { tipo: "R", cantidad: 2 },
+			{ tipo: "E", cantidad: 2 },
+        ],
+    },
+	{
+        nombre: "Fiees-Ohohtum t8",
+        imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320942123394273310/Fiees-Ohohtum_t8.png?ex=676b6eb8&is=676a1d38&hm=53123e5da956533e17c8c57f692435f62ae032cba5b5c00b9a9be9364517eb1a&=&format=webp&quality=lossless&width=475&height=350", // Ruta remota
+        recursos: [
+            { tipo: "V", cantidad: 6 },
+            { tipo: "R", cantidad: 2 },
+			{ tipo: "E", cantidad: 2 },
+        ],
+    },
+	{
+        nombre: "Files-Azazsum t8",
+        imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320942319817461922/Files-Azazsum_t8.png?ex=676b6ee6&is=676a1d66&hm=f595190cbe82fce4c47a2ba0819f4ab6fcdb135d769d30d07146a4da01480529&=&format=webp&quality=lossless&width=468&height=350", // Ruta remota
+        recursos: [
+            { tipo: "V", cantidad: 7 },
+            { tipo: "MI", cantidad: 2 },
+			{ tipo: "AL", cantidad: 2 },
+        ],
+    },
+	{
+        nombre: "Hiles-Izizaum t8",
+        imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320945023755489371/Files-Izizaum_t8.png?ex=676b716b&is=676a1feb&hm=31d6661a9e6c2e17bd2261a3578057f84fca34391b1af55b835a5a38ff74f133&=&format=webp&quality=lossless&width=851&height=640", // Ruta remota
+        recursos: [
             { tipo: "V", cantidad: 6 },
             { tipo: "M", cantidad: 2 },
+			{ tipo: "R", cantidad: 2 },
+        ],
+    },
+	{
+        nombre: "Filites-Izohun t8",
+        imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320945501192978493/Filites-Izohun_t8.png?ex=676b71dd&is=676a205d&hm=56cfb8dfbb6ebfdb3b69dabd1cc6a4c1ff26d5228fbfa9a69430e9047ba50cd3&=&format=webp&quality=lossless&width=470&height=350", // Ruta remota
+        recursos: [
+            { tipo: "A", cantidad: 1 },
+			{ tipo: "V", cantidad: 5 },
+            { tipo: "PI", cantidad: 2 },
+			{ tipo: "E", cantidad: 2 },
         ],
     },
 ];
