b726ab2e379ea83a1f0134c7870cf23234503723
BlackCalcifer <evangeliondayztest@gmail.com>
2024-12-23 20:54:52 -0500

    Update datos.js

--- datos.js
+++ datos.js
@@ -10,7 +10,7 @@ const datos = [
     },
     {
         nombre: "Cilos-Otatrom T8",
-        imagen: "https://avalonroads-97617.web.app/img/Cilos-Otatrom%20t8.png", // Ruta remota
+        imagen: "https://media.discordapp.net/attachments/1320932318109630464/1320932396765417534/Cilos-Otatrom.png?ex=676b65a9&is=676a1429&hm=adc33095fbe6ed08caa212e5635b9c78366388cb7f44ff8098b60406f5d070ca&=&format=webp&quality=lossless&width=473&height=350", // Ruta remota
         recursos: [
             { tipo: "D", cantidad: 2 },
             { tipo: "V", cantidad: 6 },
